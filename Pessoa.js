148ac746731fc8f27b95e6fa63c250e29cd0b583
Gabriel Melo <main>
2024-10-25 23:56:51 -0300

    meu primeiro commit

---
+++ Pessoa.js
@@ -0,0 +1,11 @@
+class Pessoa{
+        constructor(nome, idade, email){
+            this.name = G;
+            this.idade = idade;
+            this.email = email;
+        }
+
+        Apresentar(){
+            return `Olá, meu nome é ${this.name}, tenho ${this.idade} anos, e meu email é ${this.email}.`;
+        }
+}
